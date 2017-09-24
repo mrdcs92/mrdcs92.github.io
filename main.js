22bf549a63814687ae92dbbc91b724b5684c0e6d
mrdcs92 <mrdcs92@gmail.com>
2017-09-24 07:45:00 +0000

    added smooth scrolling

--- main.js
+++ main.js
@@ -12,6 +12,14 @@ $(document).ready(function() {
     $(".inner").css("font-family", fontChange());
   });
 
+  var $root = $("html, body");
+  $("#small-nav-items li a, #big-nav-items li a").click(function(){
+    $root.stop().animate({
+      scrollTop: $($(this).attr('href')).offset().top - 80
+    }, 1000);
+    return false;
+  });
+
   var fontChange = function() {  
     var fontList = ["Abril Fatface", "Bungee", "Bungee Inline", "Limelight", "Monoton", "Montserrat Subrayada", "Nova Square", "Playfair Display SC", "Syncopate", "Unica One"];
 
